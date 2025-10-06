988c55b7fc6e6138a9976cc07ea350c15f9e308a
Alexandre Vasconcelos <arv.alexandre@gmail.com>
2025-10-06 16:33:59 -0300

    Adicionando utilização de baseUrl via cypress.config.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe('Login', () => {
   beforeEach(() => {
-    cy.visit(Cypress.env('BASE_URL'))
+    cy.visit('/')
   })
   
   it('Login com dados válidos deve permitir entrada no sistema', () => {
